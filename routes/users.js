7c23db0543f27e9a100518f2c6a2d99c814a33d5
nelusha <nelushapcm@gmail.com>
2020-08-24 19:01:03 +0530

    passport-authentication

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 const bodyParser = require('body-parser');
 var User = require('../models/user');
 
+var passport = require('passport');
+
 var router = express.Router();
 router.use(bodyParser.json());
 
@@ -10,7 +12,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/signup', (req, res, next) => {
+/*router.post('/signup', (req, res, next) => {
   User.findOne({username: req.body.username})
   .then((user) => {
     if(user != null) {
@@ -30,9 +32,27 @@ router.post('/signup', (req, res, next) => {
     res.json({status: 'Registration Successful!', user: user});
   }, (err) => next(err))
   .catch((err) => next(err));
+}); */
+
+router.post('/signup', (req, res, next) => {
+  User.register(new User({username: req.body.username}), 
+    req.body.password, (err, user) => {
+    if(err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({err: err});
+    }
+    else {
+      passport.authenticate('local')(req, res, () => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: true, status: 'Registration Successful!'});
+      });
+    }
+  });
 });
 
-router.post('/login', (req, res, next) => {
+/*router.post('/login', (req, res, next) => {
 
   if(!req.session.user) {
     var authHeader = req.headers.authorization;
@@ -74,7 +94,13 @@ router.post('/login', (req, res, next) => {
     res.setHeader('Content-Type', 'text/plain');
     res.end('You are already authenticated!');
   }
-})
+})*/
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({success: true, status: 'You are successfully logged in!'});
+});
 
 
 router.get('/logout', (req, res) => {
